2f342ee013ee7abd39f29065f9d3e09ac09cb7d1
Grace Lemelin <glemelin99@gmail.com>
2024-03-26 14:32:53 -0400

    first commit

---
+++ Database/modules.js
@@ -0,0 +1,332 @@
+export default [
+  {
+    "_id": "M101",
+    "name": "Introduction to Rocket Propulsion",
+    "description": "Basic principles of rocket propulsion and rocket engines.",
+    "course": "RS101",
+    "lessons": [
+      {
+        "_id": "L101",
+        "name": "History of Rocketry",
+        "description": "A brief history of rocketry and space exploration.",
+        "module": "M101"
+      },
+      {
+        "_id": "L102",
+        "name": "Rocket Propulsion Fundamentals",
+        "description": "Basic principles of rocket propulsion.",
+        "module": "M101"
+      },
+      {
+        "_id": "L103",
+        "name": "Rocket Engine Types",
+        "description": "Overview of different types of rocket engines.",
+        "module": "M101"
+      }
+    ]
+  },
+  {
+    "_id": "M102",
+    "name": "Fuel and Combustion",
+    "description": "Understanding rocket fuel, combustion processes, and efficiency.",
+    "course": "RS101",
+    "lessons": [
+      {
+        "_id": "L201",
+        "name": "Rocket Fuel",
+        "description": "Overview of different types of rocket fuels.",
+        "module": "M102"
+      },
+      {
+        "_id": "L202",
+        "name": "Combustion Processes",
+        "description": "Understanding combustion processes and efficiency.",
+        "module": "M102"
+      },
+      {
+        "_id": "L203",
+        "name": "Combustion Instability",
+        "description": "Understanding combustion instability and mitigation.",
+        "module": "M102"
+      }
+    ]
+  },
+  {
+    "_id": "M103",
+    "name": "Nozzle Design",
+    "description": "Principles of rocket nozzle design and performance optimization.",
+    "course": "RS101",
+    "lessons": [
+      {
+        "_id": "L301",
+        "name": "Nozzle Design",
+        "description": "Overview of different types of rocket nozzles.",
+        "module": "M103"
+      },
+      {
+        "_id": "L302",
+        "name": "Nozzle Performance",
+        "description": "Understanding nozzle performance and efficiency.",
+        "module": "M103"
+      },
+      {
+        "_id": "L303",
+        "name": "Nozzle Optimization",
+        "description": "Optimizing nozzle design for specific applications.",
+        "module": "M103"
+      }
+    ]
+  },
+  {
+    "_id": "M201",
+    "name": "Fundamentals of Aerodynamics",
+    "description": "Basic aerodynamic concepts and fluid dynamics principles.",
+    "course": "RS102"
+  },
+  {
+    "_id": "M202",
+    "name": "Subsonic and Supersonic Flow",
+    "description": "Understanding subsonic and supersonic aerodynamic behaviors.",
+    "course": "RS102"
+  },
+  {
+    "_id": "M203",
+    "name": "Aerodynamic Heating",
+    "description": "Study of aerodynamic heating and thermal protection systems.",
+    "course": "RS102"
+  },
+  {
+    "_id": "M301",
+    "name": "Spacecraft Structural Design",
+    "description": "Fundamentals of designing spacecraft structures and materials selection.",
+    "course": "RS103"
+  },
+  {
+    "_id": "M302",
+    "name": "Orbital Mechanics",
+    "description": "Understanding orbital dynamics and mission planning.",
+    "course": "RS103"
+  },
+  {
+    "_id": "M303",
+    "name": "Spacecraft Systems Engineering",
+    "description": "Overview of spacecraft systems and subsystems engineering.",
+    "course": "RS103"
+  },
+  {
+    "_id": "A101",
+    "name": "Introduction to Algorithms",
+    "description": "Basic algorithms.",
+    "course": "CS123",
+    "lessons": [
+      {
+        "_id": "L101",
+        "name": "History of Algorithms",
+        "description": "A brief history of algorithms.",
+        "module": "A101"
+      },
+      {
+        "_id": "L102",
+        "name": "Algorithm Fundamentals",
+        "description": "Basic principles of rocket propulsion.",
+        "module": "A101"
+      },
+      {
+        "_id": "L103",
+        "name": "Basic Algorithms",
+        "description": "Overview of basic algorithms.",
+        "module": "A101"
+      }
+    ]
+  },
+  {
+    "_id": "A102",
+    "name": "Dynamic Programming",
+    "description": "Basics of dynamic programming.",
+    "course": "CS123",
+    "lessons": [
+      {
+        "_id": "L101",
+        "name": "Top Down vs Bottom Up",
+        "description": "Different ways of dynamic programming.",
+        "module": "A102"
+      }
+    ]
+  },
+  {
+    "_id": "A103",
+    "name": "Time Complexity",
+    "description": "Basics of time complexity.",
+    "course": "CS123",
+    "lessons": [
+      {
+        "_id": "L101",
+        "name": "Big O Notation",
+        "description": "Learning notation.",
+        "module": "A103"
+      }
+    ]
+  },
+  {
+    "_id": "CS101",
+    "name": "Introduction to Computer Systems",
+    "description": "Introduction to systems.",
+    "course": "CS456",
+    "lessons": [
+      {
+        "_id": "L101",
+        "name": "History of Computer Systems",
+        "description": "A brief history of computer systems.",
+        "module": "CS101"
+      },
+      {
+        "_id": "L102",
+        "name": "Turing Machines",
+        "description": "Basic principles of turing machines.",
+        "module": "CS101"
+      },
+      {
+        "_id": "L103",
+        "name": "Introduction to C",
+        "description": "Overview of C.",
+        "module": "CS101"
+      }
+    ]
+  },
+  {
+    "_id": "CS102",
+    "name": "Memory and Pointers",
+    "description": "Basics of memory usage and pointers.",
+    "course": "CS456",
+    "lessons": [
+      {
+        "_id": "L101",
+        "name": "Vim and Nano",
+        "description": "Different ways of programming in the terminal.",
+        "module": "CS102"
+      }
+    ]
+  },
+  {
+    "_id": "CS103",
+    "name": "Assembly",
+    "description": "Basics of Assembly.",
+    "course": "CS456",
+    "lessons": [
+      {
+        "_id": "L101",
+        "name": "Registers",
+        "description": "Learning the different registers.",
+        "module": "CS103"
+      }
+    ]
+  },
+  {
+    "_id": "DS101",
+    "name": "Introduction to Data Science",
+    "description": "Introduction to data science.",
+    "course": "DS100",
+    "lessons": [
+      {
+        "_id": "L101",
+        "name": "History of Data Science",
+        "description": "A brief history of data science.",
+        "module": "DS101"
+      },
+      {
+        "_id": "L102",
+        "name": "Jupyter Notebook",
+        "description": "Set up environment.",
+        "module": "DS101"
+      },
+      {
+        "_id": "L103",
+        "name": "Python",
+        "description": "Overview of Python.",
+        "module": "DS101"
+      }
+    ]
+  },
+  {
+    "_id": "DS102",
+    "name": "Libraries",
+    "description": "Basics of different common libraries.",
+    "course": "DS100",
+    "lessons": [
+      {
+        "_id": "L101",
+        "name": "Pandas",
+        "description": "Getting familiar with the Pandas library.",
+        "module": "DS102"
+      }
+    ]
+  },
+  {
+    "_id": "DS103",
+    "name": "Data Visualization",
+    "description": "Basics of Data Visualization.",
+    "course": "DS100",
+    "lessons": [
+      {
+        "_id": "L101",
+        "name": "Altair",
+        "description": "Using Altair to plot data.",
+        "module": "DS103"
+      }
+    ]
+  },
+  {
+    "_id": "IS101",
+    "name": "Introduction to Human Computer Interaction",
+    "description": "Introduction to HCI.",
+    "course": "IS300",
+    "lessons": [
+      {
+        "_id": "L101",
+        "name": "History of HCI",
+        "description": "A brief history of HCI.",
+        "module": "IS101"
+      },
+      {
+        "_id": "L102",
+        "name": "Principles of Visual Design",
+        "description": "Learning the basics of visual design.",
+        "module": "IS101"
+      },
+      {
+        "_id": "L103",
+        "name": "Fonts",
+        "description": "Overview of different fonts.",
+        "module": "IS101"
+      }
+    ]
+  },
+  {
+    "_id": "IS102",
+    "name": "Nielsen Heuristics",
+    "description": "Basics of nielsen heuristics.",
+    "course": "IS300",
+    "lessons": [
+      {
+        "_id": "L101",
+        "name": "10 Heuristics",
+        "description": "Getting familiar with the 10 Heuristics.",
+        "module": "IS102"
+      }
+    ]
+  },
+  {
+    "_id": "IS103",
+    "name": "Affordances, Signifiers, and Gulfs",
+    "description": "Learning afforances, signifiers, and gulfs.",
+    "course": "IS300",
+    "lessons": [
+      {
+        "_id": "L101",
+        "name": "Gulf of Evaluation",
+        "description": "Defining what the gulf of evaluation is.",
+        "module": "IS103"
+      }
+    ]
+  }
+]
