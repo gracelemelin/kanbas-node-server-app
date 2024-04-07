1f2bbbdccd04966cba6f384fff42a58fc49f6d59
Grace Lemelin <glemelin99@gmail.com>
2024-04-07 16:58:05 -0400

    working on a6

--- App.js
+++ App.js
@@ -3,10 +3,14 @@ import Hello from "./Hello.js"
 import Lab5 from './Lab5.js';
 import CourseRoutes from "./Courses/routes.js";
 import ModuleRoutes from "./Modules/routes.js";
+import mongoose from 'mongoose';
+import UserRoutes from './Users/routes.js';
 import cors from "cors";
+mongoose.connect("mongodb://localhost:27017");
 const app = express()
 app.use(cors());
 app.use(express.json());
+UserRoutes(app);
 ModuleRoutes(app);
 CourseRoutes(app);
 Lab5(app)
