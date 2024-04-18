004ce69486e467aaf9e83fc57e01c0480a3f64d0
Grace Lemelin <glemelin99@gmail.com>
2024-04-18 11:52:47 -0400

    made progeres

--- QuizQuestions/dao.js
+++ QuizQuestions/dao.js
@@ -2,5 +2,5 @@ import model from "./model.js";
 
 export const addQuestion = (question) => model.create(question);
 export const findQuestionsForAQuiz = (qid) => model.find({ qzid: qid });
-export const updateQuestion = (qid, question) => model.updateOne({id: qid}, {$set: question})
-export const deleteQuestion = (qid) => model.deleteOne({ id: qid });
+export const updateQuestion = (qid, question) => model.updateOne({_id: qid}, {$set: question})
+export const deleteQuestion = (qid) => model.deleteOne({ _id: qid });
