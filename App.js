b7c445cf2b1511a34901e8738232558f7c4da807
Grace Lemelin <glemelin99@gmail.com>
2024-04-20 22:03:24 -0400

    fixing issues

--- App.js
+++ App.js
@@ -11,29 +11,29 @@ import cors from "cors";
 mongoose.connect("mongodb://localhost:27017/kanbas");
 const app = express()
 app.use(cors({
-  credentials: true,
-  origin: process.env.FRONTEND_URL,
+  // credentials: true,
+  // origin: process.env.FRONTEND_URL,
 }));
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "https://a6--lustrous-platypus-b74747.netlify.app");
-  res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-  next();
-});
-const sessionOptions = {
-  secret: process.env.SESSION_SECRET,
-  resave: false,
-  saveUninitialized: false,
-};
-if (process.env.NODE_ENV !== "development") {
-  sessionOptions.proxy = true;
-  sessionOptions.cookie = {
-    sameSite: "none",
-    secure: true,
-    domain: process.env.HTTP_SERVER_DOMAIN,
-  };
-}
-app.use(session(sessionOptions));
+// app.use(function(req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "https://a6--lustrous-platypus-b74747.netlify.app");
+//   res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+//   next();
+// });
+// const sessionOptions = {
+//   secret: process.env.SESSION_SECRET,
+//   resave: false,
+//   saveUninitialized: false,
+// };
+// if (process.env.NODE_ENV !== "development") {
+//   sessionOptions.proxy = true;
+//   sessionOptions.cookie = {
+//     sameSite: "none",
+//     secure: true,
+//     domain: process.env.HTTP_SERVER_DOMAIN,
+//   };
+// }
+// app.use(session(sessionOptions));
 app.use(express.json());
 UserRoutes(app);
 ModuleRoutes(app);
